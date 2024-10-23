d0630e558f78890a42ea2d553659f8d8ec1e83b2
Lucas Peixoto <descomplicandoapratica@gmail.com>
2024-10-23 10:07:59 -0300

    fix youtube channel iframe width

--- app/meu-canal/page.tsx
+++ app/meu-canal/page.tsx
@@ -1,27 +1,4 @@
-import Image from "@/components/Image";
 import { Link } from "@nextui-org/link";
-import React from "react";
-
-const programmingLanguages = {
-  title: "Linguages de programação",
-  items: [
-    { name: "Javascript", logo: "javascript.png" },
-    { name: "Typescript", logo: "typescript.png" },
-    { name: "Java", logo: "java.png" },
-    { name: "Python", logo: "python.png" },
-  ],
-};
-
-const frameworks = {
-  title: "Frameworks",
-  items: [
-    { name: "Angular", logo: "angular.png" },
-    { name: "React", logo: "react.png" },
-    { name: "Next Js", logo: "next.png" },
-    { name: "Node", logo: "node.png" },
-    { name: "SpringBoot", logo: "springboot.png" },
-  ],
-};
 
 export default function MyChannelPage() {
   return (
@@ -35,7 +12,7 @@ export default function MyChannelPage() {
         </p>
       </div>
       <div className="container mt-8">
-        <div className="mb-16 flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
           <p className="text-base sm:text-xl md:text-xl leading-7">
             O canal Descomplicando a prática é meu novo canal do youtube onde
             pretendo trazer muito conteúdo educativo com tutoriais práticos
@@ -50,14 +27,16 @@ export default function MyChannelPage() {
         </div>
 
         {/* iframe */}
-        <div className="flex justify-center flex-col items-center mb-16 my-4 w-full">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/No4Xch9pjo4"
-            title="Apresentação canal"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          ></iframe>
+        <div className="flex justify-center flex-col items-center my-4 w-full">
+          <section className="flex w-[90%] sm:w-[70%] md:w-[50%]">
+            <iframe
+              width="100%"
+              height="280"
+              src="https://www.youtube.com/embed/No4Xch9pjo4"
+              title="Apresentação canal"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+          </section>
           <Link
             isExternal
             href="https://www.youtube.com/channel/UC6LY6Xw5ff_KaHwjHWRA9oA?sub_confirmation=1"
